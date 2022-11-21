bf9fac7de0451919c3d46fd9af2aed0a76b51684
Anni <adarvoy@gmail.com>
2022-11-21 23:27:56 +0100

    avances

--- src/main/java/com/edix/controlclientes/controller/UserHandler.java
+++ src/main/java/com/edix/controlclientes/controller/UserHandler.java
@@ -1,21 +1,57 @@
 package com.edix.controlclientes.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.edix.controlclientes.model.repository.IntUserDao;
+//import com.edix.controlclientes.model.repository.IntUserDao;
 
 @Controller
 @RequestMapping("/users")
 public class UserHandler {
-	@Autowired
-	private IntUserDao udao;
+	//@Autowired
+	//private IntUserDao udao;
 	
 	@GetMapping("/login")
-	public String logUser;
-	public int 
+		public String logInUser () {
+		//TO-DO
+		return "users";
+	}
+	
+	@GetMapping("/logout")
+		public String logOutUser () {
+		//TO-DO
+		return "users";
+	}
+	
+	@GetMapping("/actives")
+		public String exitSesion () {
+		//TO-DO
+		return "users";
+	}
+	
+	@GetMapping("/Highlighted")
+		public String getHighlightedEvents () {
+		//TO-DO
+		return "users";
+	}
+	
+	@GetMapping("/detail/{id}")
+	public String getDetail () {
+	//TO-DO
+	return "users";
+}
+	
+	 @PostMapping("/reservar/{}id")
+	 	public String sendBooking() { 
+		//TO-DO  
+		return "users";
+	 }
+	 
+	 
+	 
 	
 	
 
